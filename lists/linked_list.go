ee493f0c0a64be9458039798984c467d973d43fd
Pavlo Moyseyenko <pavel.moyseyenko@yahoo.com>
2023-10-21 18:07:11 +0300

    new: methods Get and RemoveFirst

--- lists/linked_list.go
+++ lists/linked_list.go
@@ -5,6 +5,12 @@
 // Package lists contains ordered collections and their manipulation
 package lists
 
+import "errors"
+
+var (
+	ErrIndexOutOfRange = errors.New("index out of range")
+)
+
 type LinkedList[T any] struct {
 	first *listItem[T]
 	last  *listItem[T]
@@ -39,13 +45,42 @@ func (list *LinkedList[T]) GetFirst() (T, bool) {
 	if list.first != nil {
 		return list.first.value, true
 	}
-	return new(listItem[T]).value, false
+	var res T
+	return res, false
 }
 func (list *LinkedList[T]) GetLast() (T, bool) {
 	if list.last != nil {
 		return list.last.value, true
 	}
-	return new(listItem[T]).value, false
+	var res T
+	return res, false
+}
+func (list *LinkedList[T]) Get(index int) (T, error) {
+	if index >= 0 && index < list.size {
+		for i, item := 0, list.first; item != nil; i, item = i+1, item.next {
+			if i == index {
+				return item.value, nil
+			}
+		}
+	}
+	var res T
+	return res, ErrIndexOutOfRange
+}
+func (list *LinkedList[T]) RemoveFirst() (T, bool) {
+	var res T
+	if list.first != nil {
+		res = list.first.value
+		if list.first.next != nil {
+			list.first.next.prev = nil
+			list.first = list.first.next
+		} else {
+			list.first = nil
+			list.last = nil
+		}
+		list.size--
+		return res, true
+	}
+	return res, false
 }
 func (list *LinkedList[T]) ToArray() []T {
 	result := make([]T, list.size)
--- lists/linked_list_test.go
+++ lists/linked_list_test.go
@@ -5,9 +5,151 @@
 package lists
 
 import (
+	"errors"
+	"fmt"
 	"reflect"
 	"testing"
 )
+
+func TestLinkedList_RemoveFirst(t *testing.T) {
+	list := NewLinkedList[int]()
+	list.AddLast(1)
+	list.AddLast(2)
+	list.AddLast(3)
+	expectedSize := list.Size()
+	for i := 0; i < 3; i++ {
+		actual, ok := list.RemoveFirst()
+		if !ok {
+			t.Fatal("the first element must exist")
+		}
+		expectedValue := i + 1
+		if actual != expectedValue {
+			t.Fatalf("wrong value: %v, want: %v", actual, expectedValue)
+		}
+		expectedSize--
+		if list.Size() != expectedSize {
+			t.Fatalf("wrong list size: %v, want: %v", list.Size(), expectedSize)
+		}
+	}
+	actual, ok := list.RemoveFirst()
+	if ok {
+		t.Fatal("the list must be empty")
+	}
+	if actual != 0 {
+		t.Fatalf("wrong value: %v, want: %v", actual, 0)
+	}
+}
+func TestLinkedList_RemoveFirst_before_last(t *testing.T) {
+	list := NewLinkedList[int]()
+	list.AddFirst(1)
+	list.AddLast(2)
+	if list.Size() != 2 {
+		t.Fatal("unexpected list size:", list.Size(), "expected:", 2)
+	}
+	actual, ok := list.RemoveFirst()
+	if !ok {
+		t.Fatalf("unexpected value: %v, expected: true", ok)
+	}
+	if actual != 1 {
+		t.Fatalf("expected: %v, actual: %v", 0, actual)
+	}
+	if list.Size() != 1 {
+		t.Fatalf("unexpeted list size: %v, want: %v", list.Size(), 1)
+	}
+	if list.first == nil {
+		t.Fatal("the first element must exist")
+	}
+	if list.last == nil {
+		t.Fatal("the last element must exist")
+	}
+	if list.first.prev != nil {
+		t.Fatal("the 'prev' value of the first element must be nil")
+	}
+	if list.first.next != nil {
+		t.Fatal("the 'next' value of the first element must be nil")
+	}
+	if list.last.prev != nil {
+		t.Fatal("the 'prev' value of the last element must be nil")
+	}
+	if list.last.next != nil {
+		t.Fatal("the 'next' value of the last element must be nil")
+	}
+	if list.last != list.first {
+		t.Fatal("values 'first' and 'last' must be the same")
+	}
+	last, _ := list.GetLast()
+	if last != 2 {
+		t.Fatalf("wrong last value: %v, want: %v", list, 2)
+	}
+}
+func TestLinkedList_RemoveFirst_single(t *testing.T) {
+	list := NewLinkedList[int]()
+	list.AddFirst(1)
+	if list.Size() != 1 {
+		t.Fatal("unexpected list size:", list.Size(), "expected:", 1)
+	}
+	actual, ok := list.RemoveFirst()
+	if !ok {
+		t.Fatalf("unexpected value: %v, expected: true", ok)
+	}
+	if actual != 1 {
+		t.Fatalf("expected: %v, actual: %v", 0, actual)
+	}
+	if list.Size() != 0 {
+		t.Fatalf("unexpeted list size: %v, want: %v", list.Size(), 0)
+	}
+	if list.first != nil {
+		t.Fatal("the first item should be nil, actual:", list.first)
+	}
+	if list.last != nil {
+		t.Fatal("the last value should be nil, actual:", list.last)
+	}
+}
+func TestLinkedList_RemoveFirst_empty(t *testing.T) {
+	list := NewLinkedList[int]()
+	actual, ok := list.RemoveFirst()
+	if ok {
+		t.Fatalf("unexpected value: %v, expected: false", ok)
+	}
+	if actual != 0 {
+		t.Fatalf("unexpected value: %v, expected: %v", actual, 0)
+	}
+}
+
+func TestLinkedList_Get(t *testing.T) {
+	crt := func(num int) string {
+		return fmt.Sprint("list item ", num)
+	}
+	list := NewLinkedList[string]()
+	list.AddFirst(crt(3))
+	list.AddFirst(crt(2))
+	list.AddFirst(crt(1))
+	list.AddLast(crt(4))
+	list.AddLast(crt(5))
+	if list.Size() != 5 {
+		t.Fatal("wrong list size, expected:", 5, "actual:", list.Size())
+	}
+	for i := 0; i < list.Size(); i++ {
+		actual, err := list.Get(i)
+		if err != nil {
+			t.Fatal("unexpected error:", err, "i:", i)
+		}
+		expected := crt(i + 1)
+		if actual != expected {
+			t.Fatalf("i: %d; unexpected value: '%s', expect: '%s'", i, actual, expected)
+		}
+	}
+}
+func TestLinkedList_Get_fail(t *testing.T) {
+	list := NewLinkedList[string]()
+	val, err := list.Get(-1)
+	if !errors.Is(err, ErrIndexOutOfRange) {
+		t.Fatalf("expected error: '%v', got: '%v'", ErrIndexOutOfRange, err)
+	}
+	if val != "" {
+		t.Fatalf("expected: '', actual: '%s'", val)
+	}
+}
 
 func TestLinkedList_ToArray_empty(t *testing.T) {
 	list := NewLinkedList[int]()
